c2f39012b9d4cdde3781aa52f6b80c5c0737501b
valentinarodriguezc <valentinarodriguezcarvallido@gmail.com>
2024-08-12 23:28:42 -0300

    solución-click

--- eventos.js
+++ eventos.js
@@ -1,3 +1,10 @@
+// Manejador de eventos para el botón "Saludar"
+document.getElementById('saludar-btn').addEventListener('click', function(event) {
+    event.stopPropagation();  // Detiene la propagación del evento al div
+    alert('Hola!');
+});
+
+// Manejador de eventos para el div
 document.getElementById('container').addEventListener('click', function() {
     alert('Hola! Soy el div');
 });
